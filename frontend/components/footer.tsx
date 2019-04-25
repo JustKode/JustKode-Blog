bdd2ec420af196472ad674ce3c592c43540861ef
박민재 <sobu0715@gmail.com>
2019-04-25 20:46:04 +0900

    feat: Footer Component

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -2,13 +2,23 @@ import styled from 'styled-components'
 
 const FooterBox = styled.div`
     width: 100%;
-    margin: auto;
+    text-align: center;
+    border-top: 0.3px solid #eeeeee;
+`
+
+const FooterBanner = styled.div`
+    font-size: 2rem;
+    font-weight: bold;
+`
+const FooterContext = styled.div`
+    
 `
 
 const Footer = (): JSX.Element => {
     return (
         <FooterBox>
-            Hello! I'm Header!
+            <FooterBanner>JUSTKODE</FooterBanner>
+            <FooterContext>Copyright © <strong>D.Com.</strong> All rights reserved.</FooterContext>
         </FooterBox>
     )
 }
