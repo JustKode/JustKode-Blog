06ea5d6340b041266b1d7e88482a72f3ab6885c2
박민재 <sobu0715@gmail.com>
2019-04-29 19:25:10 +0900

    fix: Model Setting #2

--- backend/board/models.py
+++ backend/board/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=20)
-    url = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
+    url = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return self.name
@@ -11,7 +11,16 @@ class Category(models.Model):
 class SubCategory(models.Model):
     name = models.CharField(max_length=20)
     url = models.CharField(max_length=20)
-    parent = models.ForeignKey(Category, on_delete=models.CASCADE)
+    parent = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategorys')
 
     def __str__(self):
         return self.parent.name + '/' + self.name
+
+    def full_name(self):
+        return self.parent.name + '/' + self.name
+
+    def full_url(self):
+        return self.parent.url + '/' + self.url
+
+    class Meta:
+        unique_together = [('url', 'parent'), ('name', 'parent')]
--- backend/post/models.py
+++ backend/post/models.py
@@ -12,41 +12,46 @@ import os
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = RichTextUploadingField()
-    summery = models.CharField(max_length=200, blank=True)
+    summary = models.CharField(max_length=200, blank=True)
     image = models.CharField(max_length=200, blank=True)
     writedAt = models.DateField(auto_now_add=True)
-    board = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    board = models.ForeignKey(SubCategory, on_delete=models.CASCADE, related_name='posts')
 
     def save(self, *args, **kwargs):
         soup = BeautifulSoup(self.content, 'html.parser')
         text = soup.text
-        imgLink = soup.select_one('img').get('src', '')
-        print(imgLink)
+        text = text.replace("\r","")
+        text = text.replace("\n"," ")
+        self.summary = text[:200]
+        imgLink = soup.select_one('img')
 
-
-        if imgLink == '':
+        if imgLink == None:
             self.image = '/static/img/no-image.jpg'
         else:
-            with open(BASE_DIR + imgLink, 'rb') as f:
-                im = Image.open(io.BytesIO(f.read()))
-                size = im.size
-                if size[0] > size[1] * 1.7:
-                    top = 0
-                    bottom = size[1]
-                    left = size[0] / 2 - size[1] * 0.85
-                    right = size[0] / 2 + size[1] * 0.85
-                else:
-                    top = size[1] / 2 - size[0] * (1 / 1.7) * 0.5
-                    bottom = size[1] / 2 + size[0] * (1 / 1.7) * 0.5
-                    left = 0
-                    right = size[0]
-                croped = im.crop((left, top, right, bottom))
-                croped.thumbnail((170, 100))
-                link = 'uploads/thumbnail/' + os.path.basename(f.name)
-                croped.convert('RGB').save(link, "JPEG", quality=200)
-                self.image = '/' + link
+            imgLink = imgLink.get('src', '')
 
-        self.summery = text[:200]
+            if imgLink == '':
+                self.image = '/static/img/no-image.jpg'
+            else:
+                with open(BASE_DIR + imgLink, 'rb') as f:
+                    im = Image.open(io.BytesIO(f.read()))
+                    size = im.size
+                    if size[0] > size[1] * 1.7:
+                        top = 0
+                        bottom = size[1]
+                        left = size[0] / 2 - size[1] * 0.85
+                        right = size[0] / 2 + size[1] * 0.85
+                    else:
+                        top = size[1] / 2 - size[0] * (1 / 1.7) * 0.5
+                        bottom = size[1] / 2 + size[0] * (1 / 1.7) * 0.5
+                        left = 0
+                        right = size[0]
+                    croped = im.crop((left, top, right, bottom))
+                    croped.thumbnail((170, 100))
+                    link = 'uploads/thumbnail/' + os.path.basename(f.name)
+                    croped.convert('RGB').save(link, "JPEG", quality=200)
+                    self.image = '/' + link
+
         super().save(*args, **kwargs)
 
     
