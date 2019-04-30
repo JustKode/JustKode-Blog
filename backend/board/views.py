d8aa186ed0bde3ac8915da6d3aea95c81e3b8dce
박민재 <sobu0715@gmail.com>
2019-04-30 18:25:48 +0900

    fix: Board API View Exception

--- backend/board/views.py
+++ backend/board/views.py
@@ -17,35 +17,46 @@ class BoardListView(APIView):
 
 class CategoryListView(APIView):
     def get(self, request, url, format=None):
-        category = Category.objects.get(url=url)
-        serializer = CategoryWithChildSerializer(instane=category)
-        return Respone(serializer.data)
+        try:
+            category = Category.objects.get(url=url)
+            serializer = CategoryWithChildSerializer(instane=category)
+            return Response(serializer.data)
+        except Category.DoesNotExist:
+            return Response({"message": "category does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
 
 class CategoryView(APIView):
     def get(self, request, url, page=1, format=None):
-        category = Category.objects.get(url=url)
-        categorySerializer = CategorySerializer(instance=category)
+        try:
+            category = Category.objects.get(url=url)
+            categorySerializer = CategorySerializer(instance=category)
 
-        posts = Post.objects.filter(board__parent=category).order_by('-writedAt')[(page-1)*10:page*10]
-        postSerializer = PostSimpleSerializer(instance=posts, many=True)
-        return Response({
-            'category': categorySerializer.data,
-            'posts': postSerializer.data
-        })
+            posts = Post.objects.filter(board__parent=category).order_by('-writedAt')[(page-1)*10:page*10]
+            postSerializer = PostSimpleSerializer(instance=posts, many=True)
+            return Response({
+                'category': categorySerializer.data,
+                'posts': postSerializer.data
+            })
+        except Category.DoesNotExist:
+            return Response({"message": "category does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
 
 class SubCategoryView(APIView):
     def get(self, request, category_url, subcategory_url, page=1, format=None):
-        category = Category.objects.get(url=category_url)
-        subcategory = SubCategory.objects.get(url=subcategory_url, parent=category)
-        subcategorySerializer = SubCategorySerializer(instance=subcategory)
+        try:
+            category = Category.objects.get(url=category_url)
+            subcategory = SubCategory.objects.get(url=subcategory_url, parent=category)
+            subcategorySerializer = SubCategorySerializer(instance=subcategory)
 
-        posts = Post.objects.filter(board=subcategory).order_by('-writedAt')[(page-1)*10:page*10]
-        postSerializer = PostSimpleSerializer(instance=posts, many=True)
-        return Response({
-            'category': subcategorySerializer.data,
-            'posts': postSerializer.data
-        })
+            posts = Post.objects.filter(board=subcategory).order_by('-writedAt')[(page-1)*10:page*10]
+            postSerializer = PostSimpleSerializer(instance=posts, many=True)
+            return Response({
+                'category': subcategorySerializer.data,
+                'posts': postSerializer.data
+            })
+        except Category.DoesNotExist:
+            return Response({"message": "category does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        except SubCategory.DoesNotExist:
+            return Response({"message": "subcategory does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
     
