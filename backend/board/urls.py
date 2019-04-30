fce409d497cee3e8dfa53cecdd5d053513a5b12f
박민재 <sobu0715@gmail.com>
2019-04-30 23:30:22 +0900

    feat: Post Serializer

--- backend/board/urls.py
+++ backend/board/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 import board.views as views
 
 urlpatterns = [
-    path('', views.BoardListView.as_view()),
-    path('<str:url>/', views.CategoryView.as_view()),
-    path('<str:url>/<int:page>/', views.CategoryView.as_view()),
-    path('<str:url>/list/', views.CategoryListView.as_view()),
-    path('<str:category_url>/<str:subcategory_url>/', views.SubCategoryView.as_view()),
-    path('<str:category_url>/<str:subcategory_url>/<int:page>/', views.SubCategoryView.as_view()),
+    path('', views.BoardListView.as_view(), name='board_list'),
+    path('<str:url>/', views.CategoryView.as_view(), name='catagory_default_view'),
+    path('<str:url>/<int:page>/', views.CategoryView.as_view(), name='category_view'),
+    path('<str:url>/list/', views.CategoryListView.as_view(), name='category_list_view'),
+    path('<str:category_url>/<str:subcategory_url>/', views.SubCategoryView.as_view(), name='subcategory_default_view'),
+    path('<str:category_url>/<str:subcategory_url>/<int:page>/', views.SubCategoryView.as_view(), name='subcategory_view'),
 ]
--- backend/post/serializer.py
+++ backend/post/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from board.models import Category, SubCategory
+from board.serializer import SubCategorySerializer
 from post.models import Post, Comment, SubComment
+
 
 class PostSimpleSerializer(serializers.ModelSerializer):
     parent_url = serializers.CharField(read_only=True, source="parent.full_url")
@@ -8,4 +10,38 @@ class PostSimpleSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Post
-        fields = ('id', 'title', 'summary', 'parent_url', 'parent_name')
+        fields = ('id', 'title', 'summary', 'image', 'parent_url', 'parent_name')
+
+
+class PostSerializer(serializers.ModelSerializer):
+    parent_url = serializers.CharField(read_only=True, source="parent.full_url")
+    parent_name = serializers.CharField(read_only=True, source="parent.full_name")
+    
+    class Meta:
+        model = Post
+        fields = ('id', 'title', 'writedAt','content', 'image', 'parent_url', 'parent_name')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('id', 'post', 'writer', 'password', 'email', 'content', 'writedAt')
+    
+
+class SubCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubComment
+        fields = ('id', 'parent', 'writer', 'password', 'email', 'content', 'writedAt')
+
+
+class CommentWithChildSerializer(serializers.ModelSerializer):
+    subcomments = serializers.SerializerMethodField()
+
+    def get_subcomments(self, instance):
+        my_subcomment = SubComment.object.filter(parent=instance)
+        return SubCommentSerializer(my_subcomment, many=True).data
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'post', 'writer', 'password', 'email', 'content', 'writedAt', 'subcomments')  
+
